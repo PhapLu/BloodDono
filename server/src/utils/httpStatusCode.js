be074c38cdfdefba27c418152f47b19d0c08fd01
ChanhPhan <phanphucchanh06102006@gmail.com>
2025-09-12 22:15:11 +0700

    Updated FE (Confirmation and Request Form)

--- server/src/utils/httpStatusCode.js
+++
@@ -1,3 +0,0 @@
-import statusCodes from "./statusCodes.js"
-import reasonPhrases from "./reasonPhrases.js"
-export {statusCodes, reasonPhrases}
--- server/src/utils/sitemapGenerator.js
+++
@@ -1,5 +0,0 @@
-import {SitemapStream, streamToPromise} from 'sitemap'
-import { createGzip } from 'zlib'
-import fs from 'fs'
-import path from 'path'
-
